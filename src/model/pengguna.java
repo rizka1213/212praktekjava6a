54c07bd6a67a51ee017b8a06124f38c51e01a185
rizka <rizkalidya361@gmail.com>
2022-06-05 10:19:15 +0800

    membuat atribut pengguna

--- src/model/pengguna.java
+++ src/model/pengguna.java
@@ -1,4 +1,10 @@
 package model;
 
 public class pengguna {
+
+    private int id;
+    private String username;
+    private String password;
+    private String namaLengkap;
+    private String level;
 }
